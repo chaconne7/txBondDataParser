2bc79fc13924ecc83663a2011ef629b370e96ca3
chaconne7 <whitekeyblackkey@comcast.net>
2016-07-07 12:53:30 -0700

    Before making major changes

--- bondScraper.py
+++ bondScraper.py
@@ -9,15 +9,28 @@ pd.set_option('display.max_rows', None)
 def get_year(filename):
     return filename[:4]
     
-def get_district_type(filename):
+def get_gov_type(filename):
     return filename[4:-4]
+
+def clean_sheet(filename, year, gov_type):
+    # merge cells in all columns after first five?
+    # remove first six rows
+    df1 = xls_file.parse(gov_type)
+
+    # insert year and governemnt type columns
+    df2 = df1.insert(3, 'Government Type', gov_type)
+    df3 = df2.insert(4, 'Year', year)
+    
+    
 
 for folder in os.listdir('TX Bond Data'):
     if not folder.startswith('.'):
         for file in os.listdir('TX Bond Data/%s' % folder):
             if not file.startswith('.'):
                 year = get_year(file)
-                district_type = get_district_type(file)
+                gov_type = get_gov_type(file)
                 filepath = 'TX Bond Data/%s/%s' % (folder, file)
                 xls_file = pd.ExcelFile(filepath)
+                df = clean_sheet(xls_file, year, district_type)
+                
                 
